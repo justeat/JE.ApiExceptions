0fc97af5259d9bddc242829192a1f6afbebc838a
Peter Mounce <peter.mounce@just-eat.com>
2015-09-23 16:33:48 +0100

    nuspec token fix

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Drop in exception stats & logging for webapi")]
+[assembly: AssemblyCompany("JUST EAT")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
